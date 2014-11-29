6ac09c41f5d01ba7925c039e106f0f37e153fd39
cordell <cordellsmail@gmail.com>
2014-11-29 17:00:25 +0100

    shamu: initial bringup for slim + small fixes
    Change-Id: I4fa432a257356eadd75911e5feb235be7f719e66

---
+++ slim.mk
@@ -0,0 +1,26 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := shamu
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := shamu
+PRODUCT_NAME := slim_shamu
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := Moto
+PRODUCT_RESTRICT_VENDOR_FILES := false
