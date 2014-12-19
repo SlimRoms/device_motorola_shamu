1faef666bba12049f0a7ec5e31c80209b90d24e7
dhacker29 <dhackerdvm@gmail.com>
2014-12-19 00:05:02 -0500

    shamu: Don't restrict vendor files
    Change-Id: I5ac39b7145dc988774bca4411b073c5373d4dde3

--- aosp_shamu.mk
+++ aosp_shamu.mk
@@ -28,7 +28,7 @@ PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Shamu
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_RESTRICT_VENDOR_FILES := true
+#PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/motorola/shamu/device.mk)
 $(call inherit-product, vendor/motorola/shamu/device-vendor.mk)
