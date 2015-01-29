Euphoria-OS/android_device_moto_shamu

ramdisk:add lkconfig

device.mk
@@ -25,7 +25,12 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu/init.shamu.power.rc:root/init.shamu.power.rc \
     device/moto/shamu/init.shamu.usb.rc:root/init.shamu.usb.rc \
     device/moto/shamu/fstab.shamu:root/fstab.shamu \
-    device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc
+    device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc 
+
+# Kernel ramdisk files
+PRODUCT_COPY_FILES += \
+    kernel/moto/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lkconfig \
+    kernel/moto/shamu/lk.ramdisk/sbin/lk-post-boot.sh:root/sbin/lk-post-boot.sh
 
 # Input device files for shamu
 PRODUCT_COPY_FILES += \

SlimRoms/device_motorola_shamu

Conflicts:
	device.mk
Change-Id: I6ac202ebf4400212926952957aa04091dd667e8e

device.mk
@@ -26,6 +26,11 @@ PRODUCT_COPY_FILES += \
     device/motorola/shamu/fstab.shamu:root/fstab.shamu \
     device/motorola/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc
 
+# Kernel ramdisk files
+PRODUCT_COPY_FILES += \
+    kernel/motorola/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lkconfig \
+    kernel/motorola/shamu/lk.ramdisk/sbin/lk-post-boot.sh:root/sbin/lk-post-boot.sh
+
 # Input device files for shamu
 PRODUCT_COPY_FILES += \
     device/motorola/shamu/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \