8b36c8d0040b351576635832fcfcb22b67691d16
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-02-02 19:09:35 -0800

    Don't use ShamuLayout
    This interferes with CM's default workspaces.
    
    Change-Id: Iffa47cf0f29151fcfedae07b6fe303da96730556

--- device.mk
+++ device.mk
@@ -183,8 +183,8 @@ PRODUCT_PACKAGES += \
     charger_res_images
 
 # for launcher layout
-PRODUCT_PACKAGES += \
-    ShamuLayout
+#PRODUCT_PACKAGES += \
+#    ShamuLayout
 
 PRODUCT_PACKAGES += \
     bdAddrLoader
