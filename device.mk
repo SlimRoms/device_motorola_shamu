Euphoria-OS/android_device_moto_shamu

ramdisk: lkconfig: symlink to "lk" for less typing

device.mk
@@ -30,6 +30,7 @@ PRODUCT_COPY_FILES += \
 # Kernel ramdisk files
 PRODUCT_COPY_FILES += \
     kernel/moto/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lkconfig \
+    kernel/moto/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lk \
     kernel/moto/shamu/lk.ramdisk/sbin/lk-post-boot.sh:root/sbin/lk-post-boot.sh
 
 # Input device files for shamu

SlimRoms/device_motorola_shamu

Conflicts:
	device.mk
Change-Id: Iee07a506a0848a7b768b65d72ab6110439c48697

device.mk
@@ -30,6 +30,7 @@ PRODUCT_COPY_FILES += \
 # Kernel ramdisk files
 PRODUCT_COPY_FILES += \
     kernel/motorola/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lkconfig \
+    kernel/motorola/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lk \
     kernel/motorola/shamu/lk.ramdisk/sbin/lk-post-boot.sh:root/sbin/lk-post-boot.sh
 
 # Input device files for shamu