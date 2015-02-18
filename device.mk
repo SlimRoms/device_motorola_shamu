Euphoria-OS/android_device_moto_shamu

ramdisk: add leanKernel core control configurator

device.mk
@@ -31,8 +31,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     kernel/moto/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lkconfig \
     kernel/moto/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lk \
+    kernel/moto/shamu/lk.ramdisk/sbin/nofreq-mpd:root/sbin/nofreq-mpd \
+    kernel/moto/shamu/lk.ramdisk/sbin/stock-mpd:root/sbin/stock-mpd \
+    kernel/moto/shamu/lk.ramdisk/sbin/lkcc:root/sbin/lkcc \
     kernel/moto/shamu/lk.ramdisk/sbin/lk-post-boot.sh:root/sbin/lk-post-boot.sh \
-    kernel/moto/shamu/lk.ramdisk/init.lk.rc:root/init.lk.rc
+    kernel/moto/shamu/lk.ramdisk/init.lk.rc:root/init.lk.rc 
+
 
 # Input device files for shamu
 PRODUCT_COPY_FILES += \

SlimRoms/device_motorola_shamu

Conflicts:
	device.mk
Change-Id: I92c91c03af635020460ccfb78a8faf2ace2b8215

device.mk
@@ -31,7 +31,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     kernel/motorola/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lkconfig \
     kernel/motorola/shamu/lk.ramdisk/sbin/lkconfig:root/sbin/lk \
-    kernel/motorola/shamu/lk.ramdisk/sbin/lk-post-boot.sh:root/sbin/lk-post-boot.sh
+    kernel/motorola/shamu/lk.ramdisk/sbin/nofreq-mpd:root/sbin/nofreq-mpd \
+    kernel/motorola/shamu/lk.ramdisk/sbin/stock-mpd:root/sbin/stock-mpd \
+    kernel/motorola/shamu/lk.ramdisk/sbin/lkcc:root/sbin/lkcc \
+    kernel/motorola/shamu/lk.ramdisk/sbin/lk-post-boot.sh:root/sbin/lk-post-boot.sh \
+    kernel/motorola/shamu/lk.ramdisk/init.lk.rc:root/init.lk.rc 
 
 # Input device files for shamu
 PRODUCT_COPY_FILES += \