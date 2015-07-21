1be46cc567daf562d454ce67d7e602c9c97f9c91
cordell <cordellsmail@gmail.com>
2015-07-21 12:22:24 +0200

    fix missing moto/motorola in boardconfig
    Change-Id: Ie6589116dde3889ed2a24eb7f4f0d74d7dd315d1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,7 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 TARGET_USES_ION := true
 TARGET_HW_DISK_ENCRYPTION := false
-TARGET_CRYPTFS_HW_PATH := device/moto/shamu/cryptfs_hw
+TARGET_CRYPTFS_HW_PATH := device/motorola/shamu/cryptfs_hw
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
