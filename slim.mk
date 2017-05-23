10335a10bb4b363889a6ea624868ceba05829bbd
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-05-23 21:12:29 +0200

    Update fingerprint to N6F27C
    Change-Id: I4e2aed12cfd8844f17b58090fff0c96b5a6cd1b7
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- slim.mk
+++ slim.mk
@@ -21,5 +21,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F26R/3582520:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F26R 3582520 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F27C/3853306:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27C 3853306 release-keys"
